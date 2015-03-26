836cee1149610da257157d2b12489b131b1e3190
julien jamet <jamet.julien@gmail.com>
2015-03-26 12:39:40 +0100

    add first

---
+++ observable.js
@@ -0,0 +1,100 @@
+/*************************************************************
+ * This script is developed by JAMET julien, http://www.bac-a-sable.com
+ * Feel free to distribute and modify code, but keep reference to its creator
+ *
+ * Observale class provides a way to define and detect event.
+ * You can define your own trigger, by function and evnet name.
+**************************************************************/
+(function( _global){
+
+    // VERSION 0.1
+    var Observable = ( function construct(){
+
+          // ajout des nouvelle fonctionnalite a l'object
+         function init( obj){
+
+            var _oEvent    = {};
+            var _aLibEvent = [];
+
+            /**
+             * [onEvent description]
+             * @param  {[string]} sEvent      [name's event]
+             * @param  {[function]} fCallBack [function call back]
+             * @return {[boolean]}
+             */
+            function onEvent( sEvent, fCallBack){
+
+                if( typeof( sEvent) == 'undefined' ||
+                    sEvent === '' ||
+                    typeof( fCallBack) == 'undefined'
+                  ){
+
+                    return false;
+                }
+
+                if( sEvent.indexOf( ' ') !== -1){
+                    aEvent = sEvent.split( ' ');
+
+                    for(var key in aEvent){
+                        var sNewEvent     = aEvent[key];
+                        _oEvent[sNewEvent] = fCallBack;
+                        _aLibEvent.push( sNewEvent);
+                    }
+
+                }else{
+                    _oEvent[sEvent] = fCallBack;
+                    _aLibEvent.push( sEvent);
+                }
+
+                return true;
+
+            }
+
+            /**
+             * [triggerEvent description]
+             * @param  {[string]} sEvent [name's event]
+             * @param  {[mix]} mArgs  [argument]
+             * @return {[boolean]}        []
+             */
+            function triggerEvent( sEvent, mArgs){
+
+                if( typeof( sEvent) == 'undefined' || sEvent === ''){
+                    return false;
+                }
+
+                var aArgs = ( Object.prototype.toString.call( mArgs ) === '[object Array]')? mArgs : [mArgs];
+
+                if( _aLibEvent.indexOf(sEvent) !== -1){
+                    _oEvent[sEvent].apply( obj, aArgs);
+                }
+
+                return true;
+            }
+
+            // add function "on"
+            Object.defineProperty( obj, 'on', {
+                      enumerable   : false,
+                      configurable : false,
+                      writable     : false,
+                      value        : onEvent
+            });
+
+            // add function "trigger"
+            Object.defineProperty( obj, 'trigger', {
+                      enumerable   : false,
+                      configurable : false,
+                      writable     : false,
+                      value        : triggerEvent
+            });
+
+         }
+
+        return {
+            init : init
+        };
+
+    })();
+
+    _global.Observable = Observable;
+
+})( window);
---
+++ script.js
@@ -0,0 +1,55 @@
+
+/*****************************************
+******************************************/
+
+var Model = function( name){
+	this.name = name;
+	this.init();
+}
+
+Model.prototype.init = function(){
+	Observable.init( this);// add obervable action
+	this.trigger('jour');
+}
+
+
+Model.prototype.move = function(){
+	this.trigger('moveEvent');
+}
+
+Model.prototype.read = function( string){
+	if( string.length > 3){
+		this.trigger('toBig', [18, 'test']);
+	}
+}
+
+Model.prototype.on = function( string){
+	console.log( 'on intern');
+}
+
+
+/*****************************************
+******************************************/
+var oModel1 = new Model( 'hello');
+var oModel2 = new Model( 'word');
+
+oModel2.on( 'moveEvent read', function(){
+	console.log( this.name+' ( move or read)');
+});
+
+oModel1.on( 'moveEvent', function(){
+	console.log( this.name + ' (move)');
+});
+
+oModel2.on( 'toBig', function( num, string){
+	this.move();
+	console.log( num);
+	console.log( string);
+});
+
+oModel1.on( 'toBig', function(){
+	console.log( 'it\'s too big');
+});
+
+
+oModel2.read('string');
